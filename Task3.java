fde1b02afcb9204413ec2dd9d6f648a0eb4744ab
Belphegoro-hub <Belphegoro030@yandex.ru>
2024-09-07 09:07:22 +0300

    второй код

---
+++ Task3.java
@@ -0,0 +1,13 @@
+package main4;
+
+public class Task3 {
+	public static void main(String[] args) {
+		String list = "Гипотенуза";
+		int length = 0;
+		
+		for(int i = 1;i<=list.length();i++) {
+				length++;
+			}
+		System.out.print(length);
+		}
+	}
